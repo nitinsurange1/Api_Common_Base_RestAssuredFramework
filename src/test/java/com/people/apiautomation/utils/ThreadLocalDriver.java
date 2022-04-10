d36f7b2334d3935d9d727e7c9c280644ca87c4a4
nitinsurange1 <nitin.s@peopleinteractive.in>
2022-04-10 22:24:23 +0530

    Create ThreadLocalDriver.java

---
+++ src/test/java/com/people/apiautomation/utils/ThreadLocalDriver.java
@@ -0,0 +1,16 @@
+package com.people.apiautomation.utils;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class ThreadLocalDriver {
+	@SuppressWarnings("rawtypes")
+	private static ThreadLocal<AndroidDriver> tlDriver = new ThreadLocal<>();
+
+	public synchronized static void setTLDriver(AndroidDriver<?> driver) {
+		tlDriver.set(driver);
+	}
+
+	public synchronized static AndroidDriver<?> getTLDriver() {
+		return tlDriver.get();
+	}
+}
